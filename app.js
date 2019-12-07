b59ee376e9aebf7c842ee490f51b63fc4b02a169
enishi <esnishi@gmail.com>
2019-12-07 14:20:58 -0300

    Teste 01 - ap01

--- app.js
+++ app.js
@@ -39,8 +39,9 @@ console.log('step 02 ');
  };
 console.log(payment_data);
  // Save and posting the payment
-    console.log(data);
+
  mercadopago.payment.save(payment_data).then(function (data) {
+       console.log('metodo - save  ');
        console.log(data);
        res.send(data);
      }).catch(function (error) {
